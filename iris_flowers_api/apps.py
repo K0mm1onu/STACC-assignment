24fa639c7c3c33b7f696eb564fd3a02e0bf36f08
Argo Kamenik <argo.kamenik@gmail.com>
2024-12-30 19:20:26 +0200

    * Created the base Django app * Django rest_framework configured and connected to PostgreSQL * Docker setup complete

---
+++ iris_flowers_api/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class IrisFlowersApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'iris_flowers_api'
+    verbose_name = 'Iris flowers API for STACC assignment'
