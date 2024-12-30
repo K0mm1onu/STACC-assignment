3c4ce6ec06162d6f5665a5a57d8ae6a8cfac4616
Argo Kamenik <argo.kamenik@gmail.com>
2024-12-30 20:31:50 +0200

    * Created data model for Iris plant specimens and species

--- iris_api/settings.py
+++ iris_api/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
+    'iris_flowers_api',
+    'rest_framework', 
     # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- iris_flowers_api/models.py
+++ iris_flowers_api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Species(models.Model):
+    name = models.CharField(max_length = 100)
+
+
+class PlantSpecimen(models.Model):
+    species = models.ForeignKey(Species, on_delete = models.CASCADE)
+    sepalLength = models.FloatField()
+    sepalWidth = models.FloatField()
+    petalLength = models.FloatField()
+    petalWidth = models.FloatField()
+    
